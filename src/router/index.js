ca6deb442a25b495e293f191b57696ee74a925ca
芳芳妞 <2276546047@qq.com>
2020-02-04 20:09:03 +0800

    首页模块 - 路由设置
    首页模块 - 路由设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 /*
  * @Date: 2020-01-29 23:16:33
- * @LastEditTime : 2020-01-30 00:24:06
+ * @LastEditTime : 2020-02-04 20:08:24
  * @Author: 李芳芳
  * @Github: https://github.com/J-Fan-Fan/
  */
@@ -9,7 +9,8 @@ import Vue from 'vue';
 // 导入路由
 // import router from 'vue-router';
 import VueRouter from 'vue-router';
-import login from '../views/login/login.vue'
+import login from '../views/login/login.vue';
+import index from '../views/index/index.vue';
 
 // 注册路由
 Vue.use(VueRouter);
@@ -20,6 +21,11 @@ const routes = [
     {
         path: '/login',
         component: login
+    },
+    // index
+    {
+        path: '/index',
+        component: index
     }
 ];
 
---
+++ src/views/index/index.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Date: 2020-02-04 19:46:39
+ * @LastEditTime : 2020-02-04 20:02:12
+ * @Author: 李芳芳
+ * @Github: https://github.com/J-Fan-Fan/
+ -->
+<template>
+<div></div>
+
+</template>
+
+<script>
+
+export default {
+  name: "login",
+  data(){},
+  methods:{}
+
+};
+</script>
+
+<style lang="less">
+
+</style>
+
